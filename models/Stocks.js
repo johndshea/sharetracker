9251096846a75ac92323bd317c2fffb8e4799220
John Shea <john@johnshea.nyc>
2015-12-15 11:51:40 -0500

    added positions array to users

--- models/Stocks.js
+++ models/Stocks.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 
 var StockSchema = new mongoose.Schema({
-  name: String,
   ticker: String,
   // might have to remove this. Added it as an afterthought.
   owner: String,
